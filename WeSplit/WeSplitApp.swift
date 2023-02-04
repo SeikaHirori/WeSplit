fe6f668768937b7034c4bd4ad6cefe66a4805a75
SeikaHirori <SeikaHirori@outlook.com>
2023-02-04 13:23:41 -0800

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Seika Hirori on 2/4/23.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
