9ee74b2850b07a5dce37ec5ed0e23c0a77df7b0b
SeikaHirori <SeikaHirori@outlook.com>
2023-02-04 19:33:21 -0800

    Finish Part 1 | Save state before purging for day 2

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -11,14 +11,20 @@ struct ContentView: View {
     let students:[String] = ["Harry", "Hermione", "Ron"]
     @State private var selectedStudent = "Harry"
     
+    let messageSelection:String = {
+        return """
+    Select your student
+    Current student:
+"""
+    }()
     
     var body:some View {
         NavigationView {
             Form {
-                Picker("Select your student", selection: $selectedStudent) {
+                Picker(messageSelection + selectedStudent, selection: $selectedStudent) {
                     ForEach(students, id:\.self) {
                         Text("\($0)")
-                    }
+                    }.padding()
                 }
             }
         }
