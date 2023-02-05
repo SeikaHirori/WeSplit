4c33d40152c4912af51904571579ba3111661af0
SeikaHirori <SeikaHirori@outlook.com>
2023-02-04 18:23:58 -0800

    Added tapCount, but it doesn't work at the moment

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,15 +8,15 @@
 import SwiftUI
 
 struct ContentView: View {
+    var tapCount = 0
+    
     var body: some View {
         NavigationView {
-            Form {
-                Section {
-                Text(helloWorld())
-                Text(anotherWorld())
-                Text(howdyFriend())
-                }
+            Button("Tap Count: \(tapCount)") {
+                tapCount += 1
             }
+
+            
             .navigationTitle("SwiftUI") // Officially adds navigation bar, so scrolling doesn't collude with upper status bar
             .navigationBarTitleDisplayMode(.inline) // This makes font smaller for "SwiftUI"
         }
